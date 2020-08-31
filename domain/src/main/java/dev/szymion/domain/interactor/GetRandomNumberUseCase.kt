004e4a14c694bfd6071492816e80d9db0a2dadf3
Jakub Szymion <jshym1@gmail.com>
2020-08-31 22:28:26 +0200

    extracted default values to constants.

--- domain/src/main/java/dev/szymion/domain/interactor/GetRandomNumberUseCase.kt
+++ domain/src/main/java/dev/szymion/domain/interactor/GetRandomNumberUseCase.kt
@@ -4,10 +4,15 @@ import java.security.SecureRandom
 import javax.inject.Inject
 
 class GetRandomNumberUseCase @Inject constructor() {
-    fun execute(from: Int = 8, to: Int = 21): Int {
+    fun execute(from: Int = DEFAULT_FROM, to: Int = DEFAULT_TO): Int {
         if (from >= to) throw IllegalArgumentException("to must be greater than from")
 
         val secureRandom = SecureRandom()
         return secureRandom.nextInt(to - from + 1) + from
+    }
+
+    companion object {
+        private const val DEFAULT_FROM = 8
+        private const val DEFAULT_TO = 21
     }
 }
