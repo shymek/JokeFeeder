b81ad8b7127aafcb85fc10e6817a5dabe656d7ab
Jakub Szymion <jshym1@gmail.com>
2020-08-31 20:46:43 +0200

    fixed detekt.

--- domain/src/test/kotlin/dev/szymion/domain/interactor/GetJokesUseCaseTest.kt
+++ domain/src/test/kotlin/dev/szymion/domain/interactor/GetJokesUseCaseTest.kt
@@ -1,12 +1,3 @@
 package dev.szymion.domain.interactor
 
-import org.junit.jupiter.api.BeforeEach
-
-import org.junit.jupiter.api.Assertions.*
-
-internal class GetJokesUseCaseTest {
-
-    @BeforeEach
-    fun setUp() {
-    }
-}
+internal class GetJokesUseCaseTest
